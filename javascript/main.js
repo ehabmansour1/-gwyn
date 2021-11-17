7c161b89bc2556ace43a59c68864d4a5947002d5
Ehab Mansour <81818344+ehabmansour1@users.noreply.github.com>
2021-11-17 14:49:42 -0800

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+let nav = document.getElementById("nav");
+let page = document.getElementById("page");
+let body = document.getElementById("body");
+function navOpen() {
+  nav.className = "nav-opened";
+  page.className = "page-down";
+  body.className = "height100";
+}
+function navClose() {
+  nav.className = "nav";
+  page.className = "page";
+  body.className = "heightNormal";
+}
+var t;
+function fullheight() {
+  t.css("height", jQuery(window).height());
+}
+window.onload = function () {
+  t = jQuery("#landing");
+  fullheight();
+};
+window.onresize = function () {
+  fullheight();
+};
